2702925e33385dbcfc8d5a8cde99587585dda6f1
Griffin Meyer <griffinmeyer@eduroam-169-233-221-155.ucsc.edu>
2016-01-29 17:59:04 -0800

    Added onclick listener to draw a square on mouse click. Need to make the square and object so that it's perminant, and then dragable.

--- simpleComposer/app/assets/javascripts/welcome.js
+++ simpleComposer/app/assets/javascripts/welcome.js
@@ -1,7 +1,48 @@
-window.onload = function(){
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+
+function main(c, ctx){
+draw(c, ctx);
+}
+
+function clear(ctx){
+ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+function draw(c, ctx){
+c.addEventListener("mousedown", getPosition, false);
+ctx.canvas.width  = window.innerWidth/2;
+ctx.canvas.height = (window.innerHeight/3)*2;
+clear(ctx);
 ctx.beginPath();
 ctx.arc(95,50,40,0,2*Math.PI);
 ctx.stroke();
+}
+
+function getPosition(e){
+	var x = e.x;
+	var y = e.y;
+
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	x -= c.offsetLeft;
+	y -= c.offsetTop;
+
+	ctx.beginPath();
+	ctx.rect(x,y,100,100);
+	ctx.closePath();
+	ctx.fill();
+}
+
+function rect(x,y,w,h){
+	ctx.beginPath();
+	ctx.rect(x,y,w,h);
+	ctx.closePath();
+	ctx.fill();
+}
+
+window.onload = function(){
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+setInterval(function(){
+	draw(c, ctx);
+}, 333);
 }
